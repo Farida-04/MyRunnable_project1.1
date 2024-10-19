81035211e97139fe210cf08ff2342b674be89ec3
Farida-04 <faridabegum872004@gmail.com>
2024-10-19 20:31:09 +0530

    Add files via upload

---
+++ MyRunnable.java
@@ -0,0 +1,29 @@
+class MyRunnable implements Runnable {
+    private String name;
+
+    public MyRunnable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + " - Count: " + i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class MultiThreadingExample {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
+        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
+
+        thread1.start();
+        thread2.start();
+    }
+}
